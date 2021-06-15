0bbaa0bc4f34c47cae525a81c39315e7f6d5b814
Sebastian Asis <sasisromero10@gmail.com>
2021-06-15 11:19:46 -0500

    :dart: IMPROVE: navbar active items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link'
 import styles from '../styles/components/Navbar.module.css'
 import ZIcon from '../components/Icon/Logo'
+import { useState } from 'react'
 export default function Navbar() {
+  const [active, setActive] = useState('home')
+  const handleActive = id => {
+    setActive(id)
+  }
+
   return (
     <div className={styles.navbarContainer}>
       <nav className={styles.navbarWrapper}>
@@ -13,24 +19,40 @@ export default function Navbar() {
           </Link>
         </div>
         <ul>
-          <li>
+          <li
+            className={`${active === 'home' && styles.active}`}
+            id="home"
+            onClick={() => handleActive('home')}
+          >
             <Link href="/">
               <a>Home</a>
             </Link>
           </li>
-          <li>
+          <li
+            className={`${active === 'explorar' && styles.active}`}
+            id="explorar"
+            onClick={() => handleActive('explorar')}
+          >
             <Link href="/explorar">
               <a>Explorar anuncio</a>
             </Link>
           </li>
-          <li>
+          <li
+            className={`${active === 'publicar' && styles.active}`}
+            id="publicar"
+            onClick={() => handleActive('publicar')}
+          >
             <Link href="/publicar">
               <a href="">Publicar</a>
             </Link>
           </li>
         </ul>
         <ul>
-          <li>
+          <li
+            className={`${active === 'iniciosesion' && styles.active}`}
+            id="iniciosesion"
+            onClick={() => handleActive('iniciosesion')}
+          >
             <Link href="/inicio-sesion">
               <a href="">Iniciar sesión</a>
             </Link>
