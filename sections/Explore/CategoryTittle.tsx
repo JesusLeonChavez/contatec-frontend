bf2b02446a5a5b893966b972a32c094b4bb0ec6c
Sebastian Asis <sasisromero10@gmail.com>
2021-06-18 16:13:57 -0500

    :dart: IMPROVE: responsive explore

--- sections/Explore/CategoryTittle.tsx
+++ sections/Explore/CategoryTittle.tsx
@@ -1,21 +1,29 @@
-import { Box, Flex, Text, Button } from "@chakra-ui/react"
+import { Box, Flex, Text, Button, Grid } from "@chakra-ui/react"
 export default function CategoryTittle() {
   return (
     <Box>
       <div className="generalWrapper">
         <Flex align="center" justify="center" py="5">
-          <Text fontSize="5xl" className="bold600">
+          <Text fontSize="5xl" className="bold600" align="center">
             Elige una catogoría
           </Text>
         </Flex>
-        <Flex justify="space-evenly" py="5">
+        <Grid
+          gap="10"
+          py="5"
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(3, 1fr)",
+            lg: "repeat(6, 1fr)"
+          }}
+        >
           <Button variant="third">Marketing</Button>
           <Button variant="third">Desarrollo web</Button>
           <Button variant="third">Programación</Button>
           <Button variant="third">Negocios</Button>
           <Button variant="third">Datos</Button>
           <Button variant="third">Diseño gráfico</Button>
-        </Flex>
+        </Grid>
       </div>
     </Box>
   )
--- sections/Explore/RecomendedService.tsx
+++ sections/Explore/RecomendedService.tsx
@@ -1,9 +1,9 @@
-import { Box, Flex, Text } from "@chakra-ui/react"
+import { Box, Flex, Text, Grid } from "@chakra-ui/react"
 import CardCategory from "../../components/CardCategory"
 
 export default function RecomendedService() {
   return (
-    <Box>
+    <Box py={{ base: "1", md: "8" }}>
       <div className="generalWrapper">
         <Flex align="center" justify="flex-start" py="5">
           <Text fontSize="3xl" className="bold500">
@@ -11,11 +11,18 @@ export default function RecomendedService() {
           </Text>
         </Flex>
 
-        <Flex justify="space-evenly">
+        <Grid
+          justify="space-evenly"
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(2, 1fr)",
+            lg: "repeat(4, 1fr)"
+          }}
+        >
           {["1", "2", "3", "4"].map(item => (
             <CardCategory key={item} title="Marketing digital" />
           ))}
-        </Flex>
+        </Grid>
       </div>
     </Box>
   )
