7cf243ad0351abaff1d7d864222a104024fa7c25
Sebastian Asis <sasisromero10@gmail.com>
2021-06-24 11:11:13 -0500

    :dart: IMPROVE: login handle simple error

--- sections/Home/Login.tsx
+++ sections/Home/Login.tsx
@@ -1,15 +1,19 @@
 import {
   Text,
+  Modal,
   ModalHeader,
+  ModalOverlay,
   ModalBody,
+  ModalContent,
   FormControl,
   FormLabel,
   FormErrorMessage,
   Input,
-  Button
+  Button,
+  useDisclosure,
+  useToast
 } from "@chakra-ui/react"
-
-import ModalCustom from "../../components/ModalCustom"
+import { useEffect } from "react"
 import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props"
 import ZIcon from "../../components/Icon/ZIcon"
 import GoogleLogin from "react-google-login"
@@ -17,15 +21,11 @@ import { useForm } from "./hooks/useForm"
 import { useErrorLogin } from "./hooks/useError"
 import { validLogin } from "./utils/valid"
 import { post } from "../../utils/http"
-// import { post } from "../../utils/http"
 
 type PropsRegister = {
   variant: string
-
   width: string
-
   showModalButtonText: string
-
   isLoading?: boolean
 }
 
@@ -34,6 +34,7 @@ export default function Login({
   width,
   showModalButtonText
 }: PropsRegister) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [values, handleInputChange, reset] = useForm({
     email: "",
     password: ""
@@ -46,17 +47,32 @@ export default function Login({
     email: "",
     password: ""
   })
+  const toast = useToast()
+  const showToast = errMessage => {
+    toast({
+      title: "Error al iniciar sesión.",
+      description: `${errMessage}`,
+      position: "top",
+      status: "error",
+      duration: 9000,
+      isClosable: true
+    })
+  }
   const handleSubmit = async e => {
     e.preventDefault()
     const { errors: errorsForm, isValid } = validLogin(values)
+    console.log(errorsForm)
     setErrors(errorsForm)
     if (isValid) {
       console.log("enviando")
-      const respLogin = await post("/api/user/login", {
+      const res = await post("/api/user/login", {
         us_correo: values.email,
         password: values.password
       })
-      console.log("respLogin: ", respLogin)
+      console.log("respLogin: ", res)
+      if (res.data.status) {
+        showToast(res.data.message)
+      }
       // setIsPosting(true)
       // const resp = await post("/api/user/register", {
       //   us_correo: values.email,
@@ -102,94 +118,101 @@ export default function Login({
       console.log("err: ", err)
     }
   }
-  return (
-    <ModalCustom
-      variant={variant}
-      width={width}
-      showModalButtonText={showModalButtonText}
-      type="register"
-      reset={reset}
-      resetError={resetErrors}
-    >
-      <ModalHeader>
-        <Text align="center" color="primary">
-          Login
-        </Text>
-      </ModalHeader>
 
-      <ModalBody color="primary">
-        <form onSubmit={handleSubmit}>
-          <FormControl mb="6" isInvalid={!!errors.email}>
-            <FormLabel>Correo electrónico</FormLabel>
-            <Input
-              type="email"
-              placeholder="P. ej. lilianasolar@gmail.com"
-              name="email"
-              onChange={handleInputChange}
-              value={email}
-            />
-            <FormErrorMessage>{errors.email}</FormErrorMessage>
-          </FormControl>
-          <FormControl mb="6" isInvalid={!!errors.password}>
-            <FormLabel>Contraseña</FormLabel>
-            <Input
-              type="password"
-              placeholder="P. ej. lilso21"
-              name="password"
-              onChange={handleInputChange}
-              value={password}
-            />
-            <FormErrorMessage>{errors.password}</FormErrorMessage>
-          </FormControl>
-          <Button width="full" variant="primary" my={2} type="submit">
-            Inicia sesión
-          </Button>
-          <Text fontSize="xs" color="primary" align="center">
-            O
-          </Text>
-          <FacebookLogin
-            appId="496057688285326"
-            autoLoad={false}
-            fields="name,email,picture"
-            // onClick={() => {}}
-            callback={res => {
-              handleFacebook(res)
-            }}
-            render={renderProps => (
-              <Button
-                leftIcon={<ZIcon name="facebookv2" />}
-                isFullWidth
-                my={2}
-                variant="facebook"
-                onClick={renderProps.onClick}
-              >
-                Inicia sesión con Facebook
+  useEffect(() => {
+    if (!isOpen) {
+      reset()
+      resetErrors()
+    }
+  }, [isOpen])
+  return (
+    <>
+      <Button variant={variant} w={width} onClick={onOpen}>
+        {showModalButtonText}
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Text align="center" color="primary">
+              Login
+            </Text>
+          </ModalHeader>
+          <ModalBody color="primary">
+            <form onSubmit={handleSubmit}>
+              <FormControl mb="6" isInvalid={!!errors.email}>
+                <FormLabel>Correo electrónico</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="P. ej. lilianasolar@gmail.com"
+                  name="email"
+                  onChange={handleInputChange}
+                  value={email}
+                />
+                <FormErrorMessage>{errors.email}</FormErrorMessage>
+              </FormControl>
+              <FormControl mb="6" isInvalid={!!errors.password}>
+                <FormLabel>Contraseña</FormLabel>
+                <Input
+                  type="password"
+                  placeholder="P. ej. lilso21"
+                  name="password"
+                  onChange={handleInputChange}
+                  value={password}
+                />
+                <FormErrorMessage>{errors.password}</FormErrorMessage>
+              </FormControl>
+              <Button width="full" variant="primary" my={2} type="submit">
+                Inicia sesión
               </Button>
-            )}
-          />
-          <GoogleLogin
-            clientId="474899151330-hdavkur1j29cii1n65o2m23h682kem04.apps.googleusercontent.com"
-            render={renderProps => (
-              <Button
-                leftIcon={<ZIcon name="google" />}
-                isFullWidth
-                my={2}
-                variant="google"
-                onClick={renderProps.onClick}
-              >
-                Inicia sesión con Google
-              </Button>
-            )}
-            onSuccess={responseGoogleS => {
-              handleGoogle(responseGoogleS)
-            }}
-            onFailure={responseGoogleF =>
-              console.log("responseGoogleF: ", responseGoogleF)
-            }
-            cookiePolicy={"single_host_origin"}
-          />
-        </form>
-      </ModalBody>
-    </ModalCustom>
+              <Text fontSize="xs" color="primary" align="center">
+                O
+              </Text>
+              <FacebookLogin
+                appId="496057688285326"
+                autoLoad={false}
+                fields="name,email,picture"
+                // onClick={() => {}}
+                callback={res => {
+                  handleFacebook(res)
+                }}
+                render={renderProps => (
+                  <Button
+                    leftIcon={<ZIcon name="facebookv2" />}
+                    isFullWidth
+                    my={2}
+                    variant="facebook"
+                    onClick={renderProps.onClick}
+                  >
+                    Inicia sesión con Facebook
+                  </Button>
+                )}
+              />
+              <GoogleLogin
+                clientId="474899151330-hdavkur1j29cii1n65o2m23h682kem04.apps.googleusercontent.com"
+                render={renderProps => (
+                  <Button
+                    leftIcon={<ZIcon name="google" />}
+                    isFullWidth
+                    my={2}
+                    variant="google"
+                    onClick={renderProps.onClick}
+                  >
+                    Inicia sesión con Google
+                  </Button>
+                )}
+                onSuccess={responseGoogleS => {
+                  handleGoogle(responseGoogleS)
+                }}
+                onFailure={responseGoogleF =>
+                  console.log("responseGoogleF: ", responseGoogleF)
+                }
+                cookiePolicy={"single_host_origin"}
+              />
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
