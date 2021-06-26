85060c3d86b164f6663e1ebde9de901011cf5b51
Sebastian Asis <sasisromero10@gmail.com>
2021-06-26 02:10:50 -0500

    :construction: FIX: clean route

--- pages/actualizar-contrasena.tsx
+++
@@ -1,9 +0,0 @@
-import PasswordReset from "../sections/Update/PasswordReset"
-
-export default function Udpate() {
-  return (
-    <div>
-      <PasswordReset />
-    </div>
-  )
-}
--- pages/usuario/resetear/[token].tsx
+++ pages/usuario/resetear/[token].tsx
@@ -1,11 +1,4 @@
 import { useRouter } from "next/router"
-
-// import { useEffect } from "react"
-
-// import { post, setAuth } from "../../../utils/http"
-
-// import { useToast } from "@chakra-ui/react"
-
 import PasswordReset from "../../../sections/Update/PasswordReset"
 
 export default function Activar() {
