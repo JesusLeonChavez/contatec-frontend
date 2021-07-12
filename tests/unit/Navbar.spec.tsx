ddf3f8a88c9f46dca500aaff35d70976e807ed66
LEONIDAS <jesus.leon1@unmsm.edu.pe>
2021-07-12 15:15:47 -0500

    unit test Navbar

---
+++ tests/unit/Navbar.spec.tsx
@@ -0,0 +1,32 @@
+/**
+
+ * @jest-environment jsdom
+
+ */
+
+import { render } from "@testing-library/react"
+
+import "@testing-library/jest-dom"
+
+import { newTheme } from "../../styles/theme"
+
+import { ChakraProvider } from "@chakra-ui/react"
+
+import Navbar from "../../components/Navbar"
+
+import React from "react"
+
+const ChakraRenderer = ({ children }) => {
+  return <ChakraProvider theme={newTheme}>{children}</ChakraProvider>
+}
+
+describe("Navbar", function () {
+  test("debe renderizarse", () => {
+    const component = render(<Navbar />, {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      wrapper: ChakraRenderer
+    })
+    expect(component).toMatchSnapshot()
+  })
+})
