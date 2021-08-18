288b7f382f09b4b18d876d3cd2ede049906c80e8
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-17 21:21:26 -0500

    :dart: Improve: agregar pasarela de pagos y consumo de endpoint

--- sections/ShowData/Pay.tsx
+++ sections/ShowData/Pay.tsx
@@ -4,9 +4,10 @@ import ModalDowload from "../ShowData/ModalDowload"
 import React, { useContext, useEffect, useState } from "react"
 import ModalSteper from "./ModalSteper"
 import { DataContext } from "../../store/GlobalState"
-import { get } from "../../utils/http"
+import { get, post } from "../../utils/http"
+import { CulqiProvider, Culqi } from "react-culqi"
 
-function PayCard({ service, user }) {
+function PayCard({ service, user, setSelectedService }) {
   const [dataOtherUser, setDataOtherUser] = useState<any>(null)
   const [postData, setPostData] = useState<any>(null)
   useEffect(() => {
@@ -40,6 +41,7 @@ function PayCard({ service, user }) {
       const { data, error } = await get(
         `/api/post/${service.msjIdPostPropuestaId}`
       )
+
       if (error) {
         console.log(error)
         return
@@ -48,7 +50,8 @@ function PayCard({ service, user }) {
       setPostData(data)
     }
     getPostData()
-  }, [service])
+  }, [])
+
   if (service.provider === "1") {
     return (
       <Flex
@@ -67,13 +70,17 @@ function PayCard({ service, user }) {
           </Text>
         </Box>
         <Flex align="center" justify="space-evenly" w="180px">
-          <ModalSteper
-            service={service}
-            user={user}
-            dataOtherUser={dataOtherUser}
-            postData={postData}
-          />
-          <button>Actualizar estado</button>
+          {service && dataOtherUser && (
+            <>
+              <ModalSteper
+                service={service}
+                user={user}
+                dataOtherUser={dataOtherUser}
+                postData={postData}
+              />
+              <button>Actualizar estado</button>
+            </>
+          )}
         </Flex>
       </Flex>
     )
@@ -96,29 +103,51 @@ function PayCard({ service, user }) {
       </Box>
       {/* Revisar renderizado condicional (es mas complejo de lo que deberia) */}
       <Flex align="center" justify="space-around" w="180px">
-        {service.trb_estado === "Contratado" ? (
-          <Button variant="primary">Pagar</Button>
-        ) : service.trb_estado === "En proceso" ? (
-          <div>
-            <h1>En proceso</h1>
-          </div>
-        ) : (
-          <div>
-            <h1>Finalizado</h1>
-          </div>
+        {/* Funcionalidad de pago! */}
+        {service && dataOtherUser && (
+          <>
+            {service.trb_estado === "Contratado" ? (
+              <Culqi>
+                {({ openCulqi, setAmount }) => {
+                  return (
+                    <Button
+                      variant="primary"
+                      onClick={async () => {
+                        await setAmount(
+                          service ? Number(service.msj_precio_prop) * 100 : 1000
+                        )
+                        await setSelectedService(service)
+                        await openCulqi()
+                      }}
+                    >
+                      Pagar
+                    </Button>
+                  )
+                }}
+              </Culqi>
+            ) : service.trb_estado === "En proceso" ? (
+              <div>
+                <h1>En proceso</h1>
+              </div>
+            ) : (
+              <div>
+                <h1>Finalizado</h1>
+              </div>
+            )}
+            <ModalSteper
+              service={service}
+              user={user}
+              dataOtherUser={dataOtherUser}
+              postData={postData}
+            />
+            <ModalDowload
+              service={service}
+              user={user}
+              dataOtherUser={dataOtherUser}
+              postData={postData}
+            />
+          </>
         )}
-        <ModalSteper
-          service={service}
-          user={user}
-          dataOtherUser={dataOtherUser}
-          postData={postData}
-        />
-        <ModalDowload
-          service={service}
-          user={user}
-          dataOtherUser={dataOtherUser}
-          postData={postData}
-        />
       </Flex>
     </Flex>
   )
@@ -126,20 +155,41 @@ function PayCard({ service, user }) {
 
 export default function Pay() {
   const { state } = useContext(DataContext)
-  const { auth, authReady } = state
+  const { auth } = state
   const [myServices, setMyServices] = useState<any[]>([])
+  const [selectedService, setSelectedService] = useState<any>(null)
+  const [isUpdatingServices, setIsUpdatingServices] = useState(false)
+
+  const handlePay = async (token: any, service: any) => {
+    console.log(token)
+    console.log(service)
+    // token.id
+    const body = {
+      pgo_nombre: auth.user.us_nombre,
+      pgo_apellido: auth.user.us_apellido,
+      pgo_token_culqi: token.id,
+      pgo_correo: token.email,
+      pgo_monto: service.msj_precio_prop,
+      pgo_trabajoId: service.trb_ID
+    }
+    const data = await post("/api/pay/service", body)
+    // Agregar toaster si hace post con exito
+    // console.log(data)
+    setSelectedService(null)
+    setIsUpdatingServices(prev => !prev)
+  }
 
   useEffect(() => {
     if (!auth?.user?.id) return
     const userData = async () => {
       const { data: total } = await get("/api/user/info")
       const { data } = total
-      console.log(data)
-      console.log("yo", auth.user)
+      // console.log(data)
+      // console.log("yo", auth.user)
       setMyServices(data)
     }
     userData()
-  }, [auth?.user?.id])
+  }, [auth?.user?.id, isUpdatingServices])
 
   return (
     <div>
@@ -151,9 +201,33 @@ export default function Pay() {
       </Text>
       <Box overflowY="scroll" h="250px">
         <Flex justify="start" direction="column">
-          {myServices.map((service, index) => (
-            <PayCard service={service} user={auth.user} key={index} />
-          ))}
+          <CulqiProvider
+            publicKey="pk_test_9824531d3eed6c8a"
+            title="Pago de servicio Contatec"
+            description="Facilitamos el servicio hacia ti"
+            onToken={e => handlePay(e, selectedService)}
+            onError={error => {
+              console.log(error)
+            }}
+            options={{
+              style: {
+                maincolor: "#FBD76D",
+                buttontext: "white",
+                maintext: "#482F51",
+                desctext: "#2B1C31"
+                // logo: "//placekitten.com/400/400"
+              }
+            }}
+          >
+            {myServices.map((service, index) => (
+              <PayCard
+                service={service}
+                user={auth.user}
+                key={index}
+                setSelectedService={setSelectedService}
+              />
+            ))}
+          </CulqiProvider>
         </Flex>
       </Box>
     </div>
