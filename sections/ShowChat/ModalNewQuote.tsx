349e00913474ed2df665154b49aaa6c266269413
Sebastian Asis <sasisromero10@gmail.com>
2021-08-18 23:05:26 -0500

    :dart: IMPROVE: validaciones finales

--- sections/ShowChat/ModalNewQuote.tsx
+++ sections/ShowChat/ModalNewQuote.tsx
@@ -64,17 +64,23 @@ export default function ModalNewQuote({
     // eslint-disable-next-line no-unused-vars
     const { errors: errorsForm, isValid } = validQuote(values, category)
     setErrors(errorsForm)
-    const dataEmit = { ...values, postId: posts.value }
-    console.log({
-      to: currentChat!.idAmiwi,
-      from: auth.user.id,
-      data: dataEmit
-    })
-    socket.emit("messageProposal", {
-      to: currentChat!.idAmiwi,
-      from: auth.user.id,
-      data: dataEmit
-    })
+    console.log(isValid)
+    if (isValid) {
+      const dataEmit = { ...values, postId: posts.value }
+      console.log({
+        to: currentChat!.idAmiwi,
+        from: auth.user.id,
+        data: dataEmit
+      })
+      socket.emit("messageProposal", {
+        to: currentChat!.idAmiwi,
+        from: auth.user.id,
+        data: dataEmit
+      })
+      setTimeout(() => {
+        onClose()
+      }, 500)
+    }
     // reset()
   }
 
