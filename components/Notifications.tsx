d8f6bcc66392b53673ddd627ac2348a33d88f9c8
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-22 17:22:06 -0500

    :dart: Improve: agregar redireccion en publicacion de posts, agregar notificaciones cuando se acepta propuesta, solucion de error al contratar

--- components/Notifications.tsx
+++ components/Notifications.tsx
@@ -1,19 +1,60 @@
 import { Box, Flex, Text } from "@chakra-ui/react"
-import ZIcon from "../components/Icon"
+import { useEffect, useContext } from "react"
 
-export default function Notifications() {
+import ZIcon from "../components/Icon"
+import { DataContext } from "../store/GlobalState"
+
+export default function Notifications({
+  recentNotifications,
+  setRecentNotifications
+}) {
+  const { state } = useContext(DataContext)
+  const { auth, socket } = state
+
+  useEffect(() => {
+    if (Object.keys(auth).length === 0) return
+    if (Object.keys(socket).length === 0) return
+
+    const functionSocket = ({ data }) => {
+      setRecentNotifications(prev => [
+        ...prev,
+        { id: data.data.trb_mensaje.id, message: data.message }
+      ])
+    }
+    socket.on("acceptYourPropose", functionSocket)
+
+    return () => {
+      socket.off("acceptYourPropose")
+    }
+  }, [auth?.user?.id, socket])
   return (
     <Box h="200" overflow="auto" minW="xl">
-      {[1, 2, 3, 4, 5, 6].map((notifElement, idx) => (
-        <Flex color="gray" align="center" py="1" minW="80" key={idx}>
-          <ZIcon name="avatar" color="primary" size={35} />
-          <Text px="4">
-            <b style={{ color: "var(--secondary)" }}>Juancho</b> - Se acaba de
-            publicar un evento:{" "}
-            <b style={{ color: "var(--secondary)" }}>Matrimonio</b>
+      {recentNotifications.length > 0 ? (
+        recentNotifications.map((notifElement, idx) => (
+          <Flex color="gray" align="center" py="1" minW="80" key={idx}>
+            <ZIcon name="avatar" color="primary" size={35} />
+            <Text px="4">
+              <b style={{ color: "var(--secondary)" }}>ID</b> -{" "}
+              {notifElement.id} -
+              <b style={{ color: "var(--secondary)" }}>
+                {notifElement.message}
+              </b>
+            </Text>
+          </Flex>
+        ))
+      ) : (
+        <Flex color="gray" align="center" py="1" minW="80">
+          <Text
+            m="0 auto"
+            textAlign="center"
+            display="flex"
+            justifyItems="center"
+            px="4"
+          >
+            No tiene notificaciones
           </Text>
         </Flex>
-      ))}
+      )}
     </Box>
   )
 }
